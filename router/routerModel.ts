79b1e108e21e1d56be3712bda36a899c00a821a0
CarloGissi <gissicarlo7@gmail.com>
2023-07-05 18:53:31 +0200

    aggiunt ifile di router

---
+++ router/routerModel.ts
@@ -0,0 +1,6 @@
+/*
+CRUD SEMPLICI
+1)inferenza tramite id scelto da postman
+2)rotta per vedere lo stato del processo durante l'inferenza
+3) rotta come due per vedere il risultato
+ */
--- router/routerUsers.ts
+++ router/routerUsers.ts
@@ -6,4 +6,11 @@ const router = express.Router()
 router.post('/nome', usContr.create)
 router.get('/tutti', usContr.getAll)
 
+
+/*
+CRUD SEMPLICI SENZA AUTENTICAZIONE
+1) l'utente deve vedere il suo credito
+2) l'utente che può ricaricare i crediti
+*/
+
 export default router
