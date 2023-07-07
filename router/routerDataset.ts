c284aa06774961e83aa3038b6ad6a2929bb05477
CarloGissi <gissicarlo7@gmail.com>
2023-07-07 16:17:42 +0200

    eliminate due cartelle

--- router/routerDataset.ts
+++
@@ -1,6 +0,0 @@
-/*
-1)Creazione di un dataset
-2)Cancellazione di un dataset
-3)Aggiornamento datset
-4)Inserimento di un immgine o un video
- */
--- router/routerModel.ts
+++
@@ -1,6 +0,0 @@
-/*
-CRUD SEMPLICI
-1)inferenza tramite id scelto da postman
-2)rotta per vedere lo stato del processo durante l'inferenza
-3) rotta come due per vedere il risultato
- */
